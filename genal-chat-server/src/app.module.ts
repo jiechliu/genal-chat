4e8db7b2919d807022de1ecb181dbe7836849192
v_jiechliu <v_jiechliu@tencent.com>
2023-05-23 21:39:42 +0800

    first

--- genal-chat-server/src/app.module.ts
+++ genal-chat-server/src/app.module.ts
@@ -5,18 +5,21 @@ import { ChatModule } from './modules/chat/chat.module';
 import { FriendModule } from './modules/friend/friend.module';
 import { GroupModule } from './modules/group/group.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       port: 3306,
+      host: '0.0.0.0',
       username: 'root',
-      password: '123456',
+      password: '000000raftx',
       database: 'chat',
       charset: "utf8mb4", // 设置chatset编码为utf8mb4
       autoLoadEntities: true,
-      synchronize: true
+      synchronize: true,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
     }),
     UserModule,
     ChatModule,
